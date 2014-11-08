8d37fc4ba47ab46bdbdd32bd97bc85d1b7526b8a
Sabina <humanoidphantom@gmail.com>
2014-11-08 14:33:18 +0300

    user change

--- backend/modules/user/views/default/view.php
+++ backend/modules/user/views/default/view.php
@@ -2,6 +2,58 @@
 /**
  * Created by PhpStorm.
  * User: Phantom
- * Date: 08.11.2014
- * Time: 0:18
- */ 
+ * Date: 26.10.2014
+ * Time: 0:28
+ */
+use yii\helpers\Html;
+use yii\widgets\DetailView;
+
+$this->registerAssetBundle('backend\assets\PostModuleAsset', \yii\web\View::POS_HEAD);
+
+$this->title = $model->name;
+$this->params['breadcrumbs'][] = ['label' => 'Пользователи', 'url' => ['index']];
+$this->params['breadcrumbs'][] = $this->title; ?>
+
+<div class="user-view padding020 widget">
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        <?= Html::a('Все пользователи', ['index'], ['class' => 'btn btn-default']) ?>
+<!--        --><?//= Html::a('Создать', ['create'], ['class' => 'btn btn-success']) ?>
+        <?= Html::a('Обновить', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
+        <?= Html::a('Удалить', ['delete', 'id' => $model->id], [
+            'class' => 'btn btn-danger',
+            'data' => [
+                'confirm' => 'Вы действительно хотите удалить запись?',
+                'method' => 'post',
+            ],
+        ]) ?>
+
+        <?= DetailView::widget([
+            'model' => $model,
+            'attributes' => [
+                'id',
+                'username',
+                'surname',
+                'name',
+                'phone',
+                'discount_card',
+                'car_number',
+                'created_at' => [
+                    'label' => 'Дата создания',
+                    'value' => date('d.m.Y, H:i',$model->created_at),
+                ],
+                'updated_at' => [
+                    'label' => 'Дата обновления',
+                    'value' => date('d.m.Y, H:i',$model->updated_at),
+                ],
+                [
+                    'attribute' => 'city',
+                    'value' => function ($model) {
+                        return $model->city;
+                    },
+                ],
+            ]
+        ]) ?>
+    </p>
+</div>
